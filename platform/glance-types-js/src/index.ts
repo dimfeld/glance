1e3293cdc48dd8d164a1c8c772ffd911a4fbb90b
Daniel Imfeld <daniel@imfeld.dev>
2023-10-19 10:56:53 -1000

    add helper to write app data

--- platform/glance-types-js/src/index.ts
+++ platform/glance-types-js/src/index.ts
@@ -1,13 +1,26 @@
 export * from './app_data.js';
 
-import envPaths from 'env-paths';
+import { promises as fs } from 'node:fs';
 import * as path from 'node:path';
+import type { AppData } from './app_data.js';
+
+import envPaths from 'env-paths';
 
 export function appPaths(appId: string) {
   const paths = envPaths('glance-dashboards', { suffix: '' });
 
   return {
-    appData: path.join(paths.data, 'app_data', `${appId}.json`),
-    appState: path.join(paths.data, 'app_state'),
+    appDataFile: path.join(paths.data, 'app_data', `${appId}.json`),
+    tmpDataDir: path.join(paths.data, 'tmp'),
+    appStateDir: path.join(paths.data, 'app_state'),
   };
+}
+
+export async function writeAppData(appId: string, appData: AppData) {
+  const { appDataFile, tmpDataDir } = appPaths(appId);
+  const tmpPath = path.join(tmpDataDir, `${appId}-${Date.now()}.json`);
+
+  // Write and rename to simulate an atomic write
+  await fs.writeFile(tmpPath, JSON.stringify(appData, null, 2));
+  await fs.rename(tmpPath, appDataFile);
 }
