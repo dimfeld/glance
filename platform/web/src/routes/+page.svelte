1497142903787b52b291de69f75db962df1c7985
Daniel Imfeld <daniel@imfeld.dev>
2023-12-01 10:06:02 -1000

    Update layout, use URL

--- platform/web/src/routes/+page.svelte
+++ platform/web/src/routes/+page.svelte
@@ -21,51 +21,61 @@
 </script>
 
 <main class="m-4">
-  <header class="w-full flex justify-end">
+  <header class="flex w-full justify-end">
     <SideLabelled label="Show dismissed" id="show_dismissed" let:id>
       <Switch {id} bind:checked={showDismissed} />
     </SideLabelled>
   </header>
-  {#each apps as { app, items } (app.id)}
-    <h2>{app.name}</h2>
-    {#each items as { id, dismissed, persistent, data } (id)}
-      <article>
-        <Card.Root>
-          <Card.Header>
-            <div class="grid grid-cols-[1fr_auto]">
-              <div>
-                {#if data.title}
-                  <Card.Title>
-                    {data.title}
-                  </Card.Title>
-                {/if}
-                {#if data.subtitle}
-                  <Card.Description>
-                    {data.subtitle}
-                  </Card.Description>
-                {/if}
-              </div>
-              {#if !persistent}
-                <form method="POST" action="?/toggle_dismissed" use:enhance>
-                  <input type="hidden" name="app_id" value={app.id} />
-                  <input type="hidden" name="item_id" value={id} />
-                  <input type="hidden" name="current_dismissed" value={dismissed} />
-                  <Button type="submit" variant="outline" size="icon">
-                    {#if dismissed}
-                      <MailOpenIcon />
-                    {:else}
-                      <MailIcon />
+  <div class="flex flex-col gap-8">
+    {#each apps as { app, items } (app.id)}
+      <section>
+        <h2 class="mb-4 text-xl">{app.name}</h2>
+        <div class="flex flex-col gap-8">
+          {#each items as { id, dismissed, persistent, data } (id)}
+            <article>
+              <Card.Root>
+                <Card.Header>
+                  <div class="grid grid-cols-[1fr_auto]">
+                    <div>
+                      {#if data.title}
+                        <Card.Title>
+                          {#if data.url}
+                            <a href={data.url} target="_blank" rel="noreferrer">{data.title}</a>
+                          {:else}
+                            {data.title}
+                          {/if}
+                        </Card.Title>
+                      {/if}
+                      {#if data.subtitle}
+                        <Card.Description>
+                          {data.subtitle}
+                        </Card.Description>
+                      {/if}
+                    </div>
+                    {#if !persistent}
+                      <form method="POST" action="?/toggle_dismissed" use:enhance>
+                        <input type="hidden" name="app_id" value={app.id} />
+                        <input type="hidden" name="item_id" value={id} />
+                        <input type="hidden" name="current_dismissed" value={dismissed} />
+                        <Button type="submit" variant="outline" size="icon">
+                          {#if dismissed}
+                            <MailOpenIcon />
+                          {:else}
+                            <MailIcon />
+                          {/if}
+                        </Button>
+                      </form>
                     {/if}
-                  </Button>
-                </form>
-              {/if}
-            </div>
-          </Card.Header>
-          {#if data.detail}
-            <Card.Content>{data.detail}</Card.Content>
-          {/if}
-        </Card.Root>
-      </article>
+                  </div>
+                </Card.Header>
+                {#if data.detail}
+                  <Card.Content>{@html data.detail}</Card.Content>
+                {/if}
+              </Card.Root>
+            </article>
+          {/each}
+        </div>
+      </section>
     {/each}
-  {/each}
+  </div>
 </main>
