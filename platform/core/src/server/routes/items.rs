7baaf0236e1634ed69324383422a50c0237c55de
Daniel Imfeld <daniel@imfeld.dev>
2024-07-29 14:09:52 -1000

    Add endpoint to upsert a single item

--- platform/core/src/server/routes/items.rs
+++ platform/core/src/server/routes/items.rs
@@ -1,9 +1,19 @@
-use axum::{extract::State, response::IntoResponse, routing::get, Json, Router};
-use axum_extra::routing::{RouterExt, TypedPath};
+use axum::{
+    extract::{Path, State},
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
+use axum_extra::{
+    extract::Query,
+    routing::{RouterExt, TypedPath},
+};
+use glance_app::AppItem;
+use http::StatusCode;
 use serde::Deserialize;
 
 use super::ServerState;
-use crate::error::Error;
+use crate::{error::Error, items::Item};
 
 async fn get_active_items(State(state): State<ServerState>) -> Result<impl IntoResponse, Error> {
     let items = state.orm.read_active_items().await?;
@@ -46,9 +56,31 @@ async fn undismiss_item(
     Ok(())
 }
 
+#[derive(Deserialize)]
+struct PostItemQuery {
+    /// If the item already exists and has been previously dismissed, undismiss it.
+    resurface: Option<bool>,
+}
+
+async fn post_item(
+    State(state): State<ServerState>,
+    Path(app_id): Path<String>,
+    Query(query): Query<PostItemQuery>,
+    Json(data): Json<AppItem>,
+) -> Result<impl IntoResponse, Error> {
+    let item = Item::from_app_item(app_id, data);
+    state
+        .orm
+        .create_or_update_item(&state.db, &item, query.resurface.unwrap_or(false))
+        .await?;
+
+    Ok(StatusCode::OK)
+}
+
 pub fn routes() -> Router<ServerState> {
     Router::new()
         .route("/active_items", get(get_active_items))
+        .route("/apps/:app_id/item", post(post_item))
         .typed_post(dismiss_item)
         .typed_post(undismiss_item)
 }
